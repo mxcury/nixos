97c29b23f07b801bc94e67af263b2b20e1ed2b5a
Tom Flattery <tomjflattery@gmail.com>
2025-10-28 20:29:56 +0000

    Builtin Catppuccin SDDM theme

--- modules/display.nix
+++ modules/display.nix
@@ -1,11 +1,30 @@
 { config, pkgs, ... }:
 
+let
+  catppuccinTheme = pkgs.catppuccin-sddm.override {
+    flavor = "mocha";
+    font = "JetBrainsMono Nerd Font Propo";
+    fontSize = "12";
+    background = "${pkgs.catppuccin-sddm}/share/sddm/themes/catppuccin-mocha/backgrounds/wall.png";
+    loginBackground = false;
+  };
+in
 {
-  # SDDM Display Manager
+  # SDDM Display Manager with Catppuccin theme
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
+    theme = "catppuccin-mocha-mauve";
+    package = pkgs.kdePackages.sddm;
   };
+
+  # Install Catppuccin SDDM theme
+  environment.systemPackages = [ catppuccinTheme ];
+  
+  # Configure SDDM to use the theme
+  systemd.tmpfiles.rules = [
+    "L+ /run/current-system/sw/share/sddm/themes/catppuccin-mocha - - - - ${catppuccinTheme}/share/sddm/themes/catppuccin-mocha"
+  ];
 
   # Enable X11 windowing system
   services.xserver.enable = true;
